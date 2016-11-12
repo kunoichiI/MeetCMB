f727715bf466428bd0ffebe67221a0fca1eb0a2f
AlwaysTPW <mingyuanwang99@gmail.com>
2016-11-11 19:56:24 -0800

    Initial Commit

---
+++ Meet CMB Team/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Meet CMB Team
+//
+//  Created by Mingyuan Wang on 11/11/16.
+//  Copyright © 2016 Mingyuan Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Meet CMB Team/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Meet CMB Team
+//
+//  Created by Mingyuan Wang on 11/11/16.
+//  Copyright © 2016 Mingyuan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
