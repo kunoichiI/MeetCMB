05146d42fb4245bc1e02a566eb4eb560160c8811
AlwaysTPW <mingyuanwang99@gmail.com>
2016-11-12 15:36:41 -0800

    align uilabels within collectionviewcell

--- Meet CMB Team/MemberViewCell.m
+++ Meet CMB Team/MemberViewCell.m
@@ -18,13 +18,17 @@
 -(id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.title = [[UILabel alloc]initWithFrame:CGRectMake(26, 125, 90, 21)];
-        self.name = [[UILabel alloc]initWithFrame:CGRectMake(11, 156, 121, 21)];
-        self.name.textColor = [UIColor blackColor];
-        self.title.textColor = [UIColor blackColor];
+        self.title = [[UILabel alloc]initWithFrame:CGRectMake(42, 154, 122, 21)];
+        self.name = [[UILabel alloc]initWithFrame:CGRectMake(42, 175, 122, 21)];
+        self.name.textColor = [UIColor whiteColor];
+        self.title.textColor = [UIColor whiteColor];
+        [self.name setFont:[UIFont fontWithName:@"HelveticaNeue" size:13.0f]];
+        [self.title setFont:[UIFont fontWithName:@"HelveticaNeue" size:13.0f]];
+        self.title.textAlignment = NSTextAlignmentCenter;
+        self.name.textAlignment = NSTextAlignmentCenter;
         
         [self.viewForFirstBaselineLayout addSubview:self.title];
-        [self.viewForLastBaselineLayout addSubview:self.name];
+        [self.viewForFirstBaselineLayout addSubview:self.name];
         
         self.viewForFirstBaselineLayout.layer.masksToBounds = YES;
     }
--- Meet CMB Team/MembersViewController.m
+++ Meet CMB Team/MembersViewController.m
@@ -57,15 +57,6 @@ static NSString * const reuseIdentifier = @"Profile";
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark <UICollectionViewDataSource>
 
